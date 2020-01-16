1137cfa8f03ce82f53357211fa41729bedd7e7ab
1lugar <bfagner@hotmail.com>
2020-01-16 00:58:28 -0300

    rota atualizada

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,9 @@
 const { Router } = require('express')
-const axios = require('axios')
-const Dev = require('./models/dev')
+const { store, index } = require('./controllers/DevController')
 
 const routes = Router()
 
-routes.post('/devs', async (req, res) => {
-  const { github_username, techs, latitude, longitude } = req.body
-
-  try {
-    const response = await axios.get(`https://api.github.com/users/${github_username}`)
-
-    const { name = login, avatar_url, bio } = response.data
-
-    const techsArray = techs.split(',').map(tech => tech.trim())
-
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude]
-    }
-
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    })
-
-    return res.json(dev)
-  } catch (error) {
-    console.error('Error', error)
-  }
-})
+routes.get('/devs', index)
+routes.post('/devs', store)
 
 module.exports = routes
